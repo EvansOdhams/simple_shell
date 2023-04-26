eeb22579e63faefa992d5d491e8ad9857609b045
EvansOdhams <evansodhams@gmail.com>
2023-04-26 19:28:21 +0300

    add Refactor shell built-in functions

--- builtin.c
+++ builtin.c
@@ -1,39 +1,51 @@
 #include "shell.h"
 
 /**
-* _myexit - set the exit status value of the program
-* @info: information about the shell and command line arguments
+* _myexit - sets the exit status value of the program
+* @info: struct containing information about the shell and
+* command line arguments
 *
 * Return: -2 to indicate that the shell should exit, or 1 if an error occurs.
 */
 int _myexit(info_t *info)
 {
-int exitcheck;
+int exit_code;
 
-if (info->argv[1])
-exitcheck = _erratoi(info->argv[1]);
+/* Check if an argument has been provided */
+if (!info->argv[1])
+{
+info->err_num = _erratoi(info->argv[1]);
+return (-2);
+}
+
+/* Convert argument to integer */
+exit_code = _erratoi(info->argv[1]);
 
 /* Check if exit code is invalid */
-if (exitcheck == -1)
+if (exit_code == -1)
 {
 info->status = 2;
-print_error(info, "Illegal number: ");
+print_error(info, "Invalid exit code: ");
 _eputs(info->argv[1]);
 _eputchar('\n');
 return (1);
 }
 
 /* Set the exit code */
-info->err_num = _erratoi(info->argv[1]);
+info->err_num = exit_code;
 return (-2);
 }
 
-
 /**
-* _mycd - change the current working directory
-* @info: information about the shell and command line arguments
-*
-* Return: always 0
+* _mycd - Change the current working directory
+* @info: A pointer to the info_t structure containing
+* info about the shell and command line arguments
+* This function changes the current working directory
+* to either the home directory, the previous directory,
+* or the directory specified in the command line arguments.
+* It also sets the value of the PWD and OLDPWD environment
+* variables.
+* Return: Always 0
 */
 int _mycd(info_t *info)
 {
@@ -42,14 +54,19 @@ int chdir_ret;
 
 /* If no argument provided, go to home directory */
 if (!info->argv[1])
+{
 chdir_ret = chdir((dir = _getenv(info, "HOME=")) ? dir : "/");
+}
 /* If argument is "-", go to previous directory */
 else if (_strcmp(info->argv[1], "-") == 0)
+{
 chdir_ret = chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+}
 /* Otherwise, go to the specified directory */
 else
+{
 chdir_ret = chdir(info->argv[1]);
-
+}
 /* Check if change directory was successful */
 if (chdir_ret == -1)
 {
@@ -57,26 +74,34 @@ print_error(info, "can't cd to ");
 _eputs(info->argv[1]), _eputchar('\n');
 }
 else
+{
 /* Set the value of the PWD and OLDPWD environment variables */
 _setenv(info, "OLDPWD", _getenv(info, "PWD="));
+}
 
 return (0);
 }
 
-
 /**
-* _myhelp - display help information
-* @info: information about the shell and command line arguments
-*
-* Return: always 0
+* _myhelp - This function alters the current directory of the procedure
+* @info: This pointer contains arguments will are used
+* to maintain constant function prototype.
+* Return: Always (0)
 */
 int _myhelp(info_t *info)
 {
-char **arg_array;
+/*  Pointer to array of arguments */
+char **args = info->argv;
 
-arg_array = info->argv;
-_puts("help call works. Function not yet implemented \n");
+/* Display message indicating that the help function is not yet implemented */
+_puts("The help function is not yet implemented.\n");
+
 if (0)
-_puts(*arg_array); /* temp att_unused workaround */
+{
+/* Temporary workaround to avoid unused variable warning */
+_puts(*args);
+}
+/* Return success */
 return (0);
 }
+
