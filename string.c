24ff9679cbb52a68ad589f68d6009ae450cf3087
EvansOdhams <evansodhams@gmail.com>
2023-04-28 16:22:27 +0300

    Code optimised

--- string.c
+++ string.c
@@ -1,75 +1,79 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
+* _strlen - Gets the length of a string.
+* @str: The string to measure.
+*
+* Return: The length of the string.
+*/
+int _strlen(char *str)
 {
-int i = 0;
+int length = 0;
 
-if (!s)
+if (!str)
 return (0);
 
-while (*s++)
-i++;
-return (i);
+while (*str++)
+length++;
+
+return (length);
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
+ * _strcmp - Compares two strings.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
  *
- * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ * Return: An integer less than, equal to, or greater than zero if s1 is found,
+ * respectively, to be less than, to match, or be greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 && *s2)
-{
-if (*s1 != *s2)
-return (*s1 - *s2);
-s1++;
-s2++;
-}
-if (*s1 == *s2)
+int i = 0;
+
+while (s1[i] && s2[i] && s1[i] == s2[i])
+i++;
+
+if (s1[i] == s2[i])
 return (0);
-else
-return (*s1 < *s2 ? -1 : 1);
+
+return (s1[i] < s2[i] ? -1 : 1);
 }
 
 /**
- * starts_with - checks if needle starts with haystack
- * @haystack: string to search
- * @needle: the substring to find
- *
- * Return: address of next char of haystack or NULL
- */
-char *starts_with(const char *haystack, const char *needle)
+* *starts_with - Checks if a string starts with a specified prefix.
+* @str: The string to search.
+* @prefix: The prefix to look for.
+*
+* Return: A pointer to the location in str where the prefix begins,
+*         or NULL if the prefix is not found.
+*/
+char *starts_with(const char *str, const char *prefix)
 {
-while (*needle)
-if (*needle++ != *haystack++)
+while (*prefix)
+if (*prefix++ != *str++)
 return (NULL);
-return ((char *)haystack);
+
+return ((char *)str);
 }
 
 /**
- * _strcat - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
+ * *_strcat - Concatenates two strings.
+ * @dest: The destination string to append to.
+ * @src: The source string to append.
  *
- * Return: pointer to destination buffer
+ * Return: A pointer to the concatenated string.
  */
 char *_strcat(char *dest, char *src)
 {
-char *ret = dest;
+char *result = dest;
 
 while (*dest)
 dest++;
 while (*src)
 *dest++ = *src++;
-*dest = *src;
-return (ret);
+*dest = '\0';
+
+return (result);
 }
+
