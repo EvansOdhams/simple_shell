469eba3afc018ad79df97bc459bbcfa2c023ec80
EvansOdhams <evansodhams@gmail.com>
2023-04-28 15:41:50 +0300

    Split_string Code optimised

--- split_string.c
+++ split_string.c
@@ -1,17 +1,15 @@
 #include "shell.h"
-
 /**
- * **strtow - splits a string into substrings based on a delimiter
- * @str: the string to split
- * @delim: the delimiter to use
- *
- * Return: a NULL-terminated array of pointers to the substrings
- */
+* **strtow - splits a string into substrings based on a delimiter
+* @str: the string to split
+* @delim: the delimiter to use
+*
+* Return: a NULL-terminated array of pointers to the substrings
+*/
 char **strtow(char *str, char *delim)
 {
 int i, j, k, m, num_words = 0;
 char **substrings;
-
 if (str == NULL || str[0] == '\0')
 return (NULL);
 if (!delim)
@@ -48,47 +46,51 @@ return (substrings);
 }
 
 /**
-* **strtow2 - splits a string into words
-* @str: the input string
-* @d: the delimeter
-* Return: a pointer to an array of strings, or NULL on failure
+* strtow2 - Splits a string into words using a delimiter.
+* @str: The string to split.
+* @delimiter: The character that delimits the words in the string.
+*
+* Return: A pointer to an array of strings, where each element
+*         points to a word. Returns NULL if str is NULL, if str is
+*         empty, or if memory allocation fails.
 */
-char **strtow2(char *str, char d)
+char **strtow2(char *str, char delimiter)
 {
-int i, j, k, m, numwords = 0;
-char **s;
-
+int i, j, k, m, num_words = 0;
+char **substrings;
 if (str == NULL || str[0] == 0)
 return (NULL);
 for (i = 0; str[i] != '\0'; i++)
-if ((str[i] != d && str[i + 1] == d) ||
-(str[i] != d && !str[i + 1]) || str[i + 1] == d)
-numwords++;
-if (numwords == 0)
-return (NULL);
-s = malloc((1 + numwords) *sizeof(char *));
-if (!s)
-return (NULL);
-for (i = 0, j = 0; j < numwords; j++)
 {
-while (str[i] == d && str[i] != d)
+if ((str[i] != delimiter && str[i + 1] == delimiter) ||
+(str[i] != delimiter && !str[i + 1]) || str[i + 1] == delimiter) {
+num_words++;
+}
+}
+if (num_words == 0)
+return (NULL);
+substrings = malloc((1 + num_words) *sizeof(char *));
+if (!substrings)
+return (NULL);
+for (i = 0, j = 0; j < num_words; j++)
+{
+while (str[i] == delimiter && str[i] != delimiter)
 i++;
 k = 0;
-while (str[i + k] != d && str[i + k] && str[i + k] != d)
+while (str[i + k] != delimiter && str[i + k] && str[i + k] != delimiter)
 k++;
-s[j] = malloc((k + 1) * sizeof(char));
-if (!s[j])
+substrings[j] = malloc((k + 1) * sizeof(char));
+if (!substrings[j])
 {
 for (k = 0; k < j; k++)
-free(s[k]);
-free(s);
+free(substrings[k]);
+free(substrings);
 return (NULL);
 }
 for (m = 0; m < k; m++)
-s[j][m] = str[i++];
-s[j][m] = 0;
+substrings[j][m] = str[i++];
+substrings[j][m] = 0;
 }
-s[j] = NULL;
-return (s);
+substrings[j] = NULL;
+return (substrings);
 }
-
