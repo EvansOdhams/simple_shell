99d52762cd23e572c0e36c107dd60cdce52bd6af
EvansOdhams <evansodhams@gmail.com>
2023-04-28 19:54:03 +0300

    code optimised

--- string.c
+++ string.c
@@ -1,116 +1,104 @@
 #include "shell.h"
 
 /**
-* _strlen - Gets the length of a string.
-* @str: The string to measure.
-*
-* Return: The length of the string.
-*/
-int _strlen(char *str)
+ * _strlen - Compute the length of a null-terminated string.
+ *
+ * @s: The string to compute the length of.
+ *
+ * Return: The length of the string, excluding the null terminator.
+ */
+size_t _strlen(const char *s)
 {
-int length = 0;
+size_t length = 0;
 
-if (!str)
+if (s == NULL)
 return (0);
 
-while (*str++)
+while (*s != '\0')
+{
 length++;
+s++;
+}
 
 return (length);
 }
 
 /**
-<<<<<<< HEAD
-* _strcmp - performs Lexical comparison of two strings.
-* @s1: the first string as it input parameter
-* @s2: the second string as its input parameters
-*
-* Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
-*/
-int my_strcmp(char *s1, char *s2)
-{
-int i = 0;
-
-while (s1[i] != '\0' && s2[i] != '\0')
-{
-if (s1[i] != s2[i])
-{
-return (s1[i] - s2[i]);
-}
-i++;
-}
-
-if (s1[i] == '\0' && s2[i] == '\0')
-{
-return (0);
-}
-else
-{
-return (s1[i] < s2[i] ? -1 : 1);
-}
-}
-
-int main(void)
-{
-char s1[] = "hello";
-char s2[] = "world";
-int result = my_strcmp(s1, s2);
-printf("%d\n", result);
-return (0);
-=======
- * _strcmp - Compares two strings.
- * @s1: The first string to be compared.
- * @s2: The second string to be compared.
+ * _strcmp - compares two strings.
+ * @str1: first string to compare.
+ * @str2: second string to compare.
  *
- * Return: An integer less than, equal to, or greater than zero if s1 is found,
- * respectively, to be less than, to match, or be greater than s2.
+ * Return: 0 if strings are equal, -1 if str1 is less than str2, 1 otherwise.
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *str1, char *str2)
 {
-int i = 0;
-
-while (s1[i] && s2[i] && s1[i] == s2[i])
-i++;
-
-if (s1[i] == s2[i])
+while (*str1 && *str2)
+{
+if (*str1 != *str2)
+return (*str1 - *str2);
+str1++;
+str2++;
+}
+if (*str1 == *str2)
 return (0);
-
-return (s1[i] < s2[i] ? -1 : 1);
->>>>>>> 24ff9679cbb52a68ad589f68d6009ae450cf3087
+else
+return (*str1 < *str2 ? -1 : 1);
 }
 
 /**
-* *starts_with - Checks if a string starts with a specified prefix.
-* @str: The string to search.
-* @prefix: The prefix to look for.
-*
-* Return: A pointer to the location in str where the prefix begins,
-*         or NULL if the prefix is not found.
-*/
-char *starts_with(const char *str, const char *prefix)
+ * starts_with - checks if a string starts with a specific prefix
+ *
+ * @haystack: the string to search
+ * @needle: the prefix to search for
+ *
+ * Return: a pointer to the first occurrence of the prefix in the string,
+ *         or NULL if the prefix is not found
+ */
+char *starts_with(const char *haystack, const char *needle)
 {
-while (*prefix)
-if (*prefix++ != *str++)
+size_t haystack_len, needle_len;
+size_t i;
+
+if (haystack == NULL || needle == NULL)
 return (NULL);
 
-return ((char *)str);
+haystack_len = strlen(haystack);
+needle_len = strlen(needle);
+
+if (needle_len > haystack_len)
+return (NULL);
+
+for (i = 0; i < needle_len; i++)
+{
+if (haystack[i] != needle[i])
+return (NULL);
+}
+
+return ((char *) haystack);
 }
 
 /**
- * *_strcat - Concatenates two strings.
- * @dest: The destination string to append to.
- * @src: The source string to append.
+ * _strcat - concatenates two strings
  *
- * Return: A pointer to the concatenated string.
+ * @dest: the destination string
+ * @src: the source string
+ *
+ * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
 char *result = dest;
 
-while (*dest)
+while (*dest != '\0')
 dest++;
-while (*src)
-*dest++ = *src++;
+
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+
 *dest = '\0';
 
 return (result);
